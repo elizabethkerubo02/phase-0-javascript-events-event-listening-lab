e384ed0b36957defb2a66012c20254b578599dc2
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-03-12 21:35:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ function addingEventListener(){const input = document.getElementById('button');
   
  input.addEventListener('click',clickAlert); 
  }
+
+ const dodger = document.getElementById("dodger"); 
+ function moveDodgerLeft(){ 
+ const leftNumbers = dodger.style.left.replace("px",""); 
+ const left=parseInt(leftNumbers,10); 
+
+  if(left>0){ 
+ dodger.style.left=`${left-1}px`; 
+ } 
+   } 
+   document.addEventListener("keydown", function (e) { 
+     if (e.key === "ArrowLeft") { 
+       moveDodgerLeft(); 
+     } 
+   }); 
+ function moveDodgerRight() { 
+   const rightNumbers = dodger.style.left.replace("px", ""); 
+   const right = parseInt(rightNumbers, 10); 
+  
+   if (right > 0) { 
+     dodger.style.left = `${right + 1}px`; 
+   } 
+   } 
+   document.addEventListener("keydown", function (e) { 
+     if (e.key === "Arrowlet") { 
+       moveDodgerRight() 
+     } 
+   } 
+   ) 
